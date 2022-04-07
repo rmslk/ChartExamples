0249bb4ce6c771e88a96fd3eac818f9573399b0b
rmslk <102646325+rmslk@users.noreply.github.com>
2022-04-07 21:40:36 +0300

    Добавление кнопки закрытия программы

--- ChartExamples.py
+++ ChartExamples.py
@@ -7,5 +7,9 @@ window = tk.Tk()
 window.geometry("450x450")
 window.title("Примеры построения графиков")
 
+# Добавление кнопки закрытия программы
+btnClose = tk.Button(window, text="Закрыть", font = ('Helvetica', 10, 'bold'))
+btnClose.place(x=330, y=400, width=90, height=30)
+
 # Запуск цикла mainloop
 window.mainloop()
