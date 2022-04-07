9f72618a42311e9c80f34ea5d5dd365c62aa119a
rmslk <102646325+rmslk@users.noreply.github.com>
2022-04-07 21:43:12 +0300

    Добавление функции закрытия окна

--- ChartExamples.py
+++ ChartExamples.py
@@ -2,13 +2,17 @@
 
 import tkinter as tk
 
+# Функция закрытия окна
+def do_close():
+    window.destroy()
+
 # Создание главного окна
 window = tk.Tk()
 window.geometry("450x450")
 window.title("Примеры построения графиков")
 
 # Добавление кнопки закрытия программы
-btnClose = tk.Button(window, text="Закрыть", font = ('Helvetica', 10, 'bold'))
+btnClose = tk.Button(window, text="Закрыть", font = ('Helvetica', 10, 'bold'), command=do_close)
 btnClose.place(x=330, y=400, width=90, height=30)
 
 # Запуск цикла mainloop
